70930b6e91dac1e9b4aa14c74ddd1a401ad8cbe8
Neo-Kekana <153192166+Neo-Kekana@users.noreply.github.com>
2025-05-30 20:53:32 +0200

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.07%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0 || x == 1) return x;
-        
+        if (x == 0 || x == 1) {
+            return x;
+        }
         int left = 1, right = x;
-        int result = 0;
-        
+        int root = 0;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-  
+            int mid = left + (right-left) / 2;
             if (mid <= x / mid) {
                 left = mid + 1;
-                result = mid; 
+                root = mid; 
             } else {
                 right = mid - 1;
             }
         }
-        
-        return result;
+        return root;
     }
 };
